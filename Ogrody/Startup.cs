b56e448a6dbb267525e6eb76b9d5a33b0e908764
Michal2123 <michalmarekowczarczyk@gmail.com>
2018-05-13 12:21:10 +0200

    sdfdf

---
+++ Ogrody/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ogrody
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment env;
+
+        public Startup(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt =>
+            {
+                if(this.env.IsProduction())
+                {
+                    opt.Filters.Add(new RequireHttpsAttribute());
+                }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg => 
+            {
+                cfg.MapRoute(
+                    "Default", "{controller}/{action}/{id?}", 
+                    new { controller = "App", Action = "Index" });
+            });
+        }
+    }
+}
